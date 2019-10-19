2a40873f9d7c5fe1c19ca1d601d361b5df19a96d
Jarl André Hübenthal <jarlah@users.noreply.github.com>
2019-10-19 09:23:39 +0200

    refactoring to my own implementation

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import * as React from "react";
-// @ts-ignore
-import {Draggable, Droppable} from 'react-drag-and-drop'
+import {useEffect, useRef} from "react";
 
 type StringKeyTypes<T> = Extract<keyof T, string>;
 
@@ -27,17 +26,59 @@ type DraggablePropsWithChildren<T, V>  = {
   children?: React.ReactNode
 } & DraggableProps<T, V>;
 
+const createDroppable = <T, V>(type: StringKeyTypes<T>) => ({ children, onDrop }: DroppablePropsWithChildren<T, V>): React.ReactElement<any> => {
+  const dropRef = useRef<HTMLDivElement>(null);
+
+  const onDropCb = (e: DragEvent) => {
+    if (e.dataTransfer) {
+      onDrop(JSON.parse(e.dataTransfer.getData(type)));
+    }
+  };
+
+  useEffect(() => {
+    const elem = dropRef.current;
+    if (elem) {
+      elem.addEventListener("drop", onDropCb);
+      return () => {
+        elem.removeEventListener("drop", onDropCb);
+      };
+    }
+  });
+
+  return (
+      <div ref={dropRef}>
+        {children}
+      </div>
+  )
+};
+
+const createDraggable = <T, V>(type: StringKeyTypes<T>) => ({ children, data }: DraggablePropsWithChildren<T, V>): React.ReactElement<any> => {
+  const dragRef = useRef<HTMLDivElement>(null);
+
+  const dragStartCb = (e: DragEvent) => {
+    if (e.dataTransfer) {
+      e.dataTransfer.setData(type, JSON.stringify(data));
+    }
+  };
+
+  useEffect(() => {
+    const elem = dragRef.current;
+    if (elem) {
+      elem.setAttribute("draggable", "true");
+      elem.addEventListener("dragstart", dragStartCb);
+    }
+  });
+
+  return (
+      <div ref={dragRef}>
+        {children}
+      </div>
+  )
+};
+
 export function dragAndDrop<V, T extends IndexedValue<T, V>>(type: StringKeyTypes<T>): TypedDroppable<T, V> {
   return {
-    Draggable: ({ children, data }: DraggablePropsWithChildren<T, V>): React.ReactElement<any> => (
-        <Draggable type={type} data={data}>
-          {children}
-        </Draggable>
-    ),
-    Droppable: ({children, onDrop}: DroppablePropsWithChildren<T, V>): React.ReactElement<any> => (
-        <Droppable types={[type]} onDrop={onDrop}>
-          {children}
-        </Droppable>
-    )
+    Draggable: createDraggable(type),
+    Droppable: createDroppable(type)
   };
 }

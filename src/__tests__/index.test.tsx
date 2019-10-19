b6201970694ee9173f15e7d639897e30c2fb5b0e
Jarl André Hübenthal <jarlah@users.noreply.github.com>
2019-10-19 09:41:29 +0200

    refactor

--- src/__tests__/index.test.tsx
+++ src/__tests__/index.test.tsx
@@ -1,26 +1,26 @@
 import { render, waitForElement } from '@testing-library/react';
 import * as React from 'react';
-import { dragAndDrop as dnd} from '../index';
+import { dragAndDrop as dnd } from '../index';
 
 test('dragAndDrop is rendering', async () => {
-    const dragAndDrop = dnd<string, { name: string }>("name");
-    const Component = () => {
-        return (
-            <>
-                <dragAndDrop.Droppable onDrop={(obj: { name: string }) => {
-                    // do something
-                }}>
-                    Drop here
-                </dragAndDrop.Droppable>
-                <dragAndDrop.Draggable data="a name">
-                    Drag this
-                </dragAndDrop.Draggable>
-            </>
-        );
-    };
+  const dragAndDrop = dnd<string, { name: string }>('name');
+  const Component = () => {
+    return (
+      <>
+        <dragAndDrop.Droppable
+          onDrop={(obj: { name: string }) => {
+            // do something
+          }}
+        >
+          Drop here
+        </dragAndDrop.Droppable>
+        <dragAndDrop.Draggable data="a name">Drag this</dragAndDrop.Draggable>
+      </>
+    );
+  };
 
-    const { getByText } = render(<Component />);
+  const { getByText } = render(<Component />);
 
-    await waitForElement(() => getByText('Drop here'));
-    await waitForElement(() => getByText('Drag this'));
+  await waitForElement(() => getByText('Drop here'));
+  await waitForElement(() => getByText('Drag this'));
 });
--- src/index.tsx
+++ src/index.tsx
@@ -1,87 +1,94 @@
-import * as React from "react";
-import {useEffect, useRef} from "react";
+import * as React from 'react';
+import { useEffect, useRef } from 'react';
 
 type StringKeyTypes<T> = Extract<keyof T, string>;
 
 type IndexedValue<T, V> = { [key in StringKeyTypes<T>]: V };
 
 type TypedDroppable<T, V> = {
-  Droppable: React.FunctionComponent<DroppableProps<T, V>>
-  Draggable: React.FunctionComponent<DraggableProps<T, V>>
+  Droppable: React.FunctionComponent<DroppableProps<T, V>>;
+  Draggable: React.FunctionComponent<DraggableProps<T, V>>;
 };
 
 type DroppableProps<T, V> = {
-  onDrop: (obj: IndexedValue<T, V>) => void
+  onDrop: (obj: IndexedValue<T, V>) => void;
 };
 
-type DroppablePropsWithChildren<T, V>  = {
-  children?: React.ReactNode
-} & DroppableProps<T, V>
+type DroppablePropsWithChildren<T, V> = {
+  children?: React.ReactNode;
+} & DroppableProps<T, V>;
 
 type DraggableProps<T, V> = {
-  data: V
-}
+  data: V;
+};
 
-type DraggablePropsWithChildren<T, V>  = {
-  children?: React.ReactNode
+type DraggablePropsWithChildren<T, V> = {
+  children?: React.ReactNode;
 } & DraggableProps<T, V>;
 
-const createDroppable = <T, V>(type: StringKeyTypes<T>) => ({ children, onDrop }: DroppablePropsWithChildren<T, V>): React.ReactElement<any> => {
+const createDroppable = <T, V>(type: StringKeyTypes<T>) => ({
+  children,
+  onDrop,
+}: DroppablePropsWithChildren<T, V>): React.ReactElement<any> => {
   const dropRef = useRef<HTMLDivElement>(null);
 
+  const dropOverCb = (e: DragEvent) => {
+    e.preventDefault();
+  };
+
   const onDropCb = (e: DragEvent) => {
+    e.preventDefault();
     if (e.dataTransfer) {
-      onDrop(JSON.parse(e.dataTransfer.getData(type)));
+      const obj = JSON.parse(e.dataTransfer.getData('source'));
+      onDrop(obj);
     }
   };
 
   useEffect(() => {
     const elem = dropRef.current;
     if (elem) {
-      elem.addEventListener("drop", onDropCb);
+      elem.addEventListener('dragover', dropOverCb);
+      elem.addEventListener('drop', onDropCb);
       return () => {
-        elem.removeEventListener("drop", onDropCb);
+        elem.removeEventListener('dragover', dropOverCb);
+        elem.removeEventListener('drop', onDropCb);
       };
     }
   });
 
-  return (
-      <div ref={dropRef}>
-        {children}
-      </div>
-  )
+  return <div ref={dropRef}>{children}</div>;
 };
 
-const createDraggable = <T, V>(type: StringKeyTypes<T>) => ({ children, data }: DraggablePropsWithChildren<T, V>): React.ReactElement<any> => {
+const createDraggable = <T, V>(type: StringKeyTypes<T>) => ({
+  children,
+  data,
+}: DraggablePropsWithChildren<T, V>): React.ReactElement<any> => {
   const dragRef = useRef<HTMLDivElement>(null);
 
   const dragStartCb = (e: DragEvent) => {
     if (e.dataTransfer) {
-      e.dataTransfer.setData(type, JSON.stringify(data));
+      const json = JSON.stringify(data);
+      e.dataTransfer.setData('source', json);
     }
   };
 
   useEffect(() => {
     const elem = dragRef.current;
     if (elem) {
-      elem.setAttribute("draggable", "true");
-      elem.addEventListener("dragstart", dragStartCb);
+      elem.setAttribute('draggable', 'true');
+      elem.addEventListener('dragstart', dragStartCb);
       return () => {
-          elem.removeEventListener("dragstart", dragStartCb);
-      }
+        elem.removeEventListener('dragstart', dragStartCb);
+      };
     }
   });
 
-  return (
-      <div ref={dragRef}>
-        {children}
-      </div>
-  )
+  return <div ref={dragRef}>{children}</div>;
 };
 
 export function dragAndDrop<V, T extends IndexedValue<T, V>>(type: StringKeyTypes<T>): TypedDroppable<T, V> {
   return {
     Draggable: createDraggable(type),
-    Droppable: createDroppable(type)
+    Droppable: createDroppable(type),
   };
 }

909c82e72f16dea585225109a32cd753741c520c
Jarl André Hübenthal <jarlah@users.noreply.github.com>
2019-10-19 13:21:03 +0200

    update example

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,17 +1,22 @@
 import React from 'react';
 import { dragAndDrop } from "typesafe-react-drag-and-drop";
 
+type Person = {
+    name: string,
+    age: number
+}
+
 const App: React.FC = () => {
-  const nameDrop = dragAndDrop<string, { name: string}>("name");
+  const nameDrop = dragAndDrop<Person, { person: Person }>("person");
   return (
     <>
-      <nameDrop.Droppable onDrop={(obj: { name: string}) => {
-        alert(`Hello, ${obj.name}`);
+      <nameDrop.Droppable onDrop={(obj: { person: Person}) => {
+        alert(`Hello, ${obj.person.name} are ${obj.person.age} years old`);
       }}>
         Drop here
       </nameDrop.Droppable>
 
-      <nameDrop.Draggable data="Jarl">
+      <nameDrop.Draggable data={{ name: "you", age: 100 }}>
         Drag me
       </nameDrop.Draggable>
     </>
